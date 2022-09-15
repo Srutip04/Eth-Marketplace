27681967704d088bd26a8118ccc2a793b2304b8d
Srutip04 <subuhishruti75@gmail.com>
2022-09-15 23:32:51 +0530

    get course data

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import {  Hero, Breadcrumbs } from "@components/common";
+import {  Hero } from "@components/common";
 import { CourseList } from "@components/course";
 import { BaseLayout } from "@components/layout";
-import { OrderCard } from "@components/order";
-import { EthRates, Walletbar } from "@components/web3";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Home() {
+export default function Home({ courses }) {
   return (
     <>
       <Hero />
-      <Breadcrumbs />
-      <Walletbar />
-      <EthRates />
-      <OrderCard />
+      {JSON.stringify(courses)}
       <CourseList />
     </>
   );
 }
-
+export function getStaticProps() {
+  const { data } = getAllCourses();
+  return {
+    props: {
+      courses: data,
+    },
+  };
+}
 Home.Layout = BaseLayout;
