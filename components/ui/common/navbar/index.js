0666bc5d2634918bacd9718e3e289264a5b7986e
Srutip04 <subuhishruti75@gmail.com>
2022-09-21 21:59:09 +0530

    disable bttn

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,14 +2,16 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 import { Button } from "@components/ui/common";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
+   const router = useRouter();
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -38,7 +40,13 @@ export default function Navbar() {
               ) : isWeb3Loaded ? (
                 <Button onClick={connect}>Connect</Button>
               ) : (
-                <Button onClick={connect}>Install Metamask</Button>
+                <Button
+                  onClick={() =>
+                    router.push("https://metamask.io/download.html")
+                  }
+                >
+                  Install Metamask
+                </Button>
               )}
             </div>
           </div>
