b74fde1375c2af6b5df49e50723ea9d9fe4499f8
Srutip04 <subuhishruti75@gmail.com>
2022-09-21 21:48:34 +0530

    loading bttn

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,9 +1,10 @@
 
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
+import { Button } from "@components/ui/common";
 
 export default function Navbar() {
-  const { connect, isWeb3Loaded } = useWeb3();
+  const { connect, isLoading, isWeb3Loaded } = useWeb3();
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
@@ -32,20 +33,12 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              {isWeb3Loaded ? (
-                <span
-                  onClick={connect}
-                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Connect
-                </span>
+              {isLoading ? (
+                <Button onClick={connect}>Loading...</Button>
+              ) : isWeb3Loaded ? (
+                <Button onClick={connect}>Connect</Button>
               ) : (
-                <span
-                  onClick={connect}
-                  className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Connect Install Metamask
-                </span>
+                <Button onClick={connect}>Install Metamask</Button>
               )}
             </div>
           </div>
