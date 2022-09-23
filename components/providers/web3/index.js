e454ecca1407ad005ddab0b37f4abf19d8dccbb2
Srutip04 <subuhishruti75@gmail.com>
2022-09-23 22:41:26 +0530

    useHooks

--- components/providers/web3/index.js
+++ components/providers/web3/index.js
@@ -46,7 +46,7 @@ export default function Web3Provider({ children }) {
    return {
      ...web3Api,
      isWeb3Loaded: web3 != null,
-     hooks: setupHooks(web3),
+     getHooks: () => setupHooks(web3),
      connect: provider
        ? async () => {
            try {
@@ -70,4 +70,10 @@ export default function Web3Provider({ children }) {
 
 export function useWeb3() {
   return useContext(Web3Context);
+}
+
+export function useHooks(cb) {
+  const { getHooks } = useWeb3();
+  const hooks = getHooks();
+  return cb(hooks);
 }
