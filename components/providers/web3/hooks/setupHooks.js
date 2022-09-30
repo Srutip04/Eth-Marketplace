34e8d1b8fdfc99c156cf8e9195ef9da4b7fd1ef7
Srutip04 <subuhishruti75@gmail.com>
2022-09-30 23:11:26 +0530

    useNetwork

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -1,5 +1,6 @@
 import { handler as createUseAccount } from "./useAccount";
-
+import { handler as createAccountHook } from "./useAccount";
+import { handler as createNetworkHook } from "./useNetwork";
 export const setupHooks = (...deps) => {
   return {
     useAccount: createUseAccount(...deps),
