904382041e23631ce1ab9f96b1855361b2168998
Srutip04 <subuhishruti75@gmail.com>
2022-09-30 19:03:24 +0530

    fetching data using swr

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,22 +1,26 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-export const handler = (web3,provider) => () => {
-  const [account, setAccount] = useState(null);
+export const handler = (web3, provider) => () => {
+  
+
+  const { mutate, ...rest } = useSWR(() => {
+      web3 ? "web3/accounts" : null,
+      async () => {
+        const accounts = await web3.eth.getAccounts();
+        return accounts[0];
+      };
+    });
 
   useEffect(() => {
-    const getAccount = async () => {
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
-    };
-    web3 && getAccount();
-  }, [web3]);
+    provider &&
+      provider.on("accountsChanged", (accounts) => mutate(accounts[0] ?? null));
+  }, [provider]);
 
-   useEffect(() => {
-     provider &&
-       provider.on("accountsChanged", (accounts) =>
-         setAccount(accounts[0] ?? null)
-       );
-   }, [provider]);
-
-  return { account };
+  return {
+    account: {
+      mutate,
+      ...rest,
+    },
+  };
 };
