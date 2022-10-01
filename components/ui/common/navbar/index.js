a8c9dd882c1fb7672571cff6d5adaed6f31de682
Srutip04 <subuhishruti75@gmail.com>
2022-10-01 14:42:55 +0530

    Display address working

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -2,12 +2,15 @@
 import { useWeb3 } from "@components/providers";
 import Link from "next/link";
 import { Button } from "@components/ui/common";
-import { useAccount } from "@components/web3/hooks/useAccount";
+import { useAccount } from "@components/hooks/web3/useAccount";
+import { useRouter } from "next/router";
 
 
 export default function Navbar() {
    const { connect, isLoading, isWeb3Loaded } = useWeb3();
    const { account } = useAccount();
+    const { pathname } = useRouter();
+
 
   return (
     <section>
@@ -20,7 +23,7 @@ export default function Navbar() {
                   Home
                 </a>
               </Link>
-              <Link href="/">
+              <Link href="/marketplace">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                   Marketplace
                 </a>
@@ -62,7 +65,7 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      {account.data && (
+      {account.data && !pathname.includes("/marketplace") && (
         <div className="flex justify-end pt-1 sm:px-6 lg:px-8">
           <div className="text-white bg-indigo-600 rounded-md p-2">
             {account.data}
