93ae72b02ee430ab1eeaa2f9d18b0c1a565a0be7
Srutip04 <subuhishruti75@gmail.com>
2022-10-09 18:51:43 +0530

    eth hook

--- components/hooks/useEthPrice.js
+++ components/hooks/useEthPrice.js
@@ -3,14 +3,14 @@ import useSWR from "swr";
 const URL =
   "https://api.coingecko.com/api/v3/coins/ethereum?localization=false&tickers=false&community_data=false&developer_data=false&sparkline=false";
 
-const fetcher = async url =>{
-    const res = await fetch(url);
-    const json  = await res.json();
-}
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const json = await res.json();
+  return json.market_data.current_price.usd ?? null;
+};
 
 export const useEthPrice = () => {
-    useSWR(
-        URL,
-        fetcher
-    )
-}
+  const swrRes = useSWR(URL, fetcher, { refreshInterval: 1000 });
+
+  return { eth: { ...swrRes } };
+};
