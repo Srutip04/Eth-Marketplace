7e3aa333374fbee3eb9e94de66e32d2160847e3b
Srutip04 <subuhishruti75@gmail.com>
2022-10-10 22:14:23 +0530

    disable can purchase bttn

--- components/ui/course/card/index.js
+++ components/ui/course/card/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Card({ course, Footer}) {
+export default function Card({ course,disabled, Footer}) {
   return (
     <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
       <div className="flex h-full">
         <div className="flex-1 h-full next-image-wrapper">
           <Image
-            className="object-cover"
+            className={`object-cover ${disabled && "filter grayscale"}`}
             src={course.coverImage}
             layout="responsive"
             width="200"
@@ -24,7 +24,9 @@ export default function Card({ course, Footer}) {
               {course.title}
             </a>
           </Link>
-          <p className="mt-2 text-gray-500">{course.description.substring(0,70)}...</p>
+          <p className="mt-2 text-gray-500">
+            {course.description.substring(0, 70)}...
+          </p>
           {Footer && <Footer />}
         </div>
       </div>
